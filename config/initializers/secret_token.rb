a365b6175184d4ffbb4f9132cd12af7989621f18
teacplusplus <teacplusplus@gmail.com>
2012-04-16 22:09:42 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'db151f841271cc02be18abbde4bf1903bc1e7cb8d13d9b36fab16ee44ebf54735d2b6da6bcfe280aaa0e4c2f7b76ecd7ebe5a8bdd1c706fa2056d99e69650e5f'
